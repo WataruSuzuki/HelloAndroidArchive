fb95e7c08a7ed8a6a39db6eccb588dfa25f24204
WataruSuzuki <wataru0406@gmail.com>
2017-01-28 08:30:00 +0900

    Create aar module

---
+++ hellolibrary/src/main/java/com/devjchankchan/hellolibrary/StudyAar.java
@@ -0,0 +1,15 @@
+package com.devjchankchan.hellolibrary;
+
+import android.util.Log;
+
+/**
+ * Created by WataruSuzuki on 2017/02/01.
+ */
+
+public class StudyAar {
+    public static String TAG = "StudyAar";
+    public void printHelloAndroidArchive() {
+        Log.i(TAG, "printHelloAndroidArchive: " + "(・∀・)");
+    }
+}
+
